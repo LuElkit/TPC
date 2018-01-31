aee104587feef00e2a6acd1a82bf664359ab5d09
Océane Merlo <oceane.merlo@r01p02.vj304.sm.cri.epita.net>
2018-01-31 13:09:03 +0100

    0

--- 20180129_TCP_client/get_page.c
+++ 20180129_TCP_client/get_page.c
@@ -1,25 +1,29 @@
+# define _XOPEN_SOURCE 700
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-
-
+#include <unistd.h>
+#include<err.h>
 char* build_query(const char *url, size_t *len)
 {
 	char *query= malloc(sizeof(char)*strlen(url)+20);
   strcpy(query, "GET ");
 	strcat(query, url);
   strcat(query," HTTP/1.0\n\r\n\r");
-  len= strlen(query);
+  *len = strlen(query);
  	return query;
 	
 }
 void get_page(const char *name, const char *url, const char *port)
 {
 	struct addrinfo hints;
-	struct addrinfo *result;
+	struct addrinfo *result,*answer;
+	int addrinfo_error;
+  int connection;
 	memset(&hints,0,sizeof(struct addrinfo));
 	hints.ai_family = AF_INET;
 	hints.ai_socktype = SOCK_STREAM;
@@ -32,9 +36,9 @@ void get_page(const char *name, const char *url, const char *port)
   }
 	for(answer= result; answer !=NULL; answer= answer->ai_next)
 	{
-		connection=socket(answer->ai-family,answer->ai_socktype,answer->ai_protocol);
+		connection=socket(answer->ai_family,answer->ai_socktype,answer->ai_protocol);
 		if(connection == -1)continue;
-		if(connect(connection,connection->ai_addr, connection->ai_addrlen)==0)break;
+		if(connect(connection,answer->ai_addr, answer->ai_addrlen)==0)break;
 		close(connection);
 	}
 	freeaddrinfo(result);
@@ -42,13 +46,22 @@ void get_page(const char *name, const char *url, const char *port)
 	{
 		errx(EXIT_FAILURE, "Couldn't connect");
 	}
-
-	size_t len;
+  size_t len;
 	char *query= build_query(url,&len);
-	
+	ssize_t ok= send(connection,query,len,0);
+	if(ok==-1)
+	{
+		err(3,"Send of infos failed");
+	}
+	char donnees[len];
+	ok= read(connection,donnees,len);
+	ok= write(STDOUT_FILENO,donnees,len);
+	close(connection);
+	free(query);
+ 	ok= write(STDOUT_FILENO,"Connection by the server",25);
 
 }
-int main(int argc, char** argv)
+/*int main(int argc, char** argv)
 {
   if(argc < 2)
 		return 0;
@@ -58,4 +71,8 @@ int main(int argc, char** argv)
   free(query);
 
 	return 0;
+}*/
+int main() {
+  get_page("perdu.com", "http://perdu.com", "80");
+  return 0;
 }
