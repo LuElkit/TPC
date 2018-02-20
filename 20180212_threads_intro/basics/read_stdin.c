6c7f415969bcd9a490208805ccc965ef28a0d4b5
Océane Merlo <oceane.merlo@r01p02.vj306.sm.cri.epita.net>
2018-02-20 12:07:03 +0100

    rendu

--- 20180212_threads_intro/basics/read_stdin.c
+++ 20180212_threads_intro/basics/read_stdin.c
@@ -2,4 +2,52 @@
 #include <stdlib.h>
 #include <err.h>
 #include <pthread.h>
+#include <unistd.h>
+#include <errno.h>
+void echo(size_t id) {
+  int r;
+  char buf[256];
+  while ( (r = read(STDIN_FILENO, buf, 255)) ) {
+    if (r == -1) {
+      if (errno == EINTR) continue;
+      err(2, "Issue reading from stdin in thread %zu", id);
+    }
+    buf[r] = 0;
+    printf("<%02zu>: %s", id, buf);
+  }
+}
 
+void* Thread(void* arg)
+{
+	size_t i = (size_t)arg;
+	echo(i);
+	pthread_exit(NULL);
+}
+int main(int argc, char** argv)
+{
+	if(argc<2)
+	{
+		printf("few arguments\n");
+		return 0;
+	}
+	if(argc>2)
+	{	
+		printf("to much arguments");
+		return 0;
+	}
+	size_t number = atoi(argv[1]);
+	pthread_t thr[number];
+	for(size_t i=0; i<number;i++)
+	{
+		int e=pthread_create(&thr[i],NULL,Thread,(void*)i);
+		if(e!=0)
+			errx(EXIT_FAILURE, "pthread_create()");
+	}
+	for(size_t j=0; j<number;j++)
+	{
+		int e= pthread_join(thr[j],NULL);
+		if(e!=0)
+			errx(EXIT_FAILURE,"pthread_join()");
+	}
+	pthread_exit(NULL);
+}
